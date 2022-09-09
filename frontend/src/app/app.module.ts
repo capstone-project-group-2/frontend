6c49520bbb6a723827917231a4024c9c079b82c4
RohanBabu17 <rohanperingathara@gmail.com>
2022-09-09 09:47:17 +0530

    updated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ReturnpolicyComponent } from './returnpolicy/returnpolicy.component';
@@ -78,7 +78,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
 
-    // NgbModule,
+    NgbModule,
     ReactiveFormsModule,
     HttpClientModule,
 
